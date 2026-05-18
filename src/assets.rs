3a0b56a04366b0887c586c84183093a9044dca6e
CosmicPredator <baranikumars2003@gmail.com>
2026-05-18 10:28:38 +0530

    good first commit

---
+++ src/assets.rs
@@ -0,0 +1,22 @@
+use gpui::{AssetSource, SharedString};
+
+pub struct Assets;
+
+impl AssetSource for Assets {
+    fn load(&self, path: &str) -> anyhow::Result<Option<std::borrow::Cow<'static, [u8]>>> {
+        std::fs::read(path)
+            .map(Into::into)
+            .map_err(Into::into)
+            .map(Some)
+    }
+
+    fn list(&self, path: &str) -> anyhow::Result<Vec<SharedString>> {
+        Ok(std::fs::read_dir(path)?
+            .filter_map(|entry| {
+                Some(SharedString::from(
+                    entry.ok()?.path().to_string_lossy().into_owned(),
+                ))
+            })
+            .collect::<Vec<_>>())
+    }
+}
